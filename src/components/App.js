ce6fadd6a50c092cab59312fd916a8fcb6570989
Ethan Lee <eyechan01@gmail.com>
2020-10-02 12:39:58 -0400

    hi

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import CivicInfo from "./civic-info/CivicInfo";
+import GeoInfo from "./geoinfo/geoinfo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <CivicInfo/>
+        <GeoInfo/>
       </header>
     </div>
   );
