499332143860ad99abc82229b199f6d41f64235e
Kev-Y-Huang <kev.y.huang@gmail.com>
2020-10-03 12:25:06 -0500

    Styling to fit other components

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,31 +4,31 @@ import CivicInfo from "./civic-info/CivicInfo";
 import GeoInfo from "./geoinfo/geoinfo";
 import VotrCrwd from "./../assets/VotrCrwd.jpg";
 import {Box} from "@material-ui/core";
-import {Link} from "react-router-dom";
 
 
 class Homepage extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-				
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-	render() {
-		return (
-			<div style={{ backgroundColor: "rgba(255, 255, 255, 0.75)" }}>
-				<Box width={700}>
-					<h1 style={{fontSize: 80}}>Welcome to</h1>
-					<img src={VotrCrwd} alt={"logo"}/>
-					<h4>The All-American way to Vote.</h4>
-					<h5>Type in your address to find the nearest polling station near you!</h5>
-					<CivicInfo/>
-				</Box>
-				<GeoInfo/>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div style={{backgroundColor: "rgba(255, 255, 255, 0.75)", width: 1500}}>
+        <Box width={"100%"}
+             justify="center"
+             alignItems="center"
+				>
+          <h1 style={{fontSize: 80}}>Welcome to</h1>
+          <img src={VotrCrwd} alt={"logo"}/>
+          <h4>The All-American way to Vote.</h4>
+          <h5>Type in your address to find the nearest polling station near you!</h5>
+          <CivicInfo/>
+        </Box>
+        <GeoInfo/>
+      </div>
+    );
+  }
 }
 
 export default Homepage;
