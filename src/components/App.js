a0e02708d4d1c307c1e37d7deab7cfa7834f6aa8
Lucas <lucaspingpao@gmail.com>
2020-10-03 05:47:50 +0200

    Configured react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -4,27 +4,32 @@ import CivicInfo from "./civic-info/CivicInfo";
 import GeoInfo from "./geoinfo/geoinfo";
 import VotrCrwd from "./../assets/VotrCrwd.jpg";
 import {Box} from "@material-ui/core";
+import {Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Box width={700}>
-          <img src={VotrCrwd} alt={"logo"}/>
-          <CivicInfo/>
-        </Box>
-        <GeoInfo/>
-      </header>
+    <Switch>
+      <Route exact path="/">
+        <div className="App">
+          <header className="App-header">
+            <Box width={700}>
+              <img src={VotrCrwd} alt={"logo"}/>
+              <CivicInfo/>
+            </Box>
+            <GeoInfo/>
+          </header>
 
-      {/* <!-- The core Firebase JS SDK is always required and must be listed first --> */}
-      <script src="/__/firebase/7.22.0/firebase-app.js"/>
-      {/* <!-- TODO: Add SDKs for Firebase products that you want to use
-          https://firebase.google.com/docs/web/setup#available-libraries --> */}
-      <script src="/__/firebase/7.22.0/firebase-analytics.js"/>
-      {/* <!-- Initialize Firebase --> */}
-      <script src="/__/firebase/init.js"/>
-    </div>
-
+          {/* <!-- The core Firebase JS SDK is always required and must be listed first --> */}
+          <script src="/__/firebase/7.22.0/firebase-app.js"/>
+          {/* <!-- TODO: Add SDKs for Firebase products that you want to use
+              https://firebase.google.com/docs/web/setup#available-libraries --> */}
+          <script src="/__/firebase/7.22.0/firebase-analytics.js"/>
+          {/* <!-- Initialize Firebase --> */}
+          <script src="/__/firebase/init.js"/>
+        </div>
+      </Route>
+      <Route exact path="/about">This is the about page</Route>
+    </Switch>
 
   );
 }
