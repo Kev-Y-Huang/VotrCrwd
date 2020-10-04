f5cc90cba0777d2cf6afa9d0c66c677873a8da83
avcheng <alexcheng@college.harvard.edu>
2020-10-03 19:02:40 -0600

    add capacity %

--- src/components/cards/LocationCard.js
+++ src/components/cards/LocationCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Card, CardActions, CardContent, CardHeader, Collapse, IconButton, Typography} from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import {FirebaseContext} from "../Firebase"
 
 export default function LocationCard(props) {
   const [expanded, setExpanded] = React.useState(false);
@@ -15,6 +16,23 @@ export default function LocationCard(props) {
     <li key={index}>{time}</li>
   );
 
+  const haversine = (lat1, lon1, lat2, lon2) => { 
+    var rEarth = Math.sin(6371); 
+    var temp = Math.sin((lat2-lat1)*(Math.PI/180)/2)*Math.sin((lat2-lat1)*(Math.PI/180)/2) + 
+    Math.cos(lat1*(Math.PI/180))*Math.cos(lat2*(Math.PI/180)) * 
+    Math.sin((lon2-lon1)*(Math.PI/180)/2)*Math.sin((lon2-lon1)*(Math.PI/180)/2); 
+    return rEarth*(Math.atan2(Math.sqrt(temp), Math.sqrt(1-temp))); 
+  };  
+  const personCounter = (lat, lon) => { 
+    let counter = 0; 
+    for (var key in props.firebase.locations()){ 
+      if(haversine(key.latitude, key.longitude, lat, lon) < 2){ 
+        counter ++; 
+      } 
+    } 
+    return counter/50*100; 
+  }
+
   return (
     <Card>
       <CardHeader
@@ -26,7 +44,10 @@ export default function LocationCard(props) {
           {props.location.address.line1}
           <br/>
           {props.location.address.city.concat(", ", props.location.address.state, " ", props.location.address.zip)}
+          <br/>
+          Capacity: {personCounter(props.location.latitude, props.location.longitude)}%
         </Typography>
+
       </CardContent>
       <CardActions disableSpacing>
         <IconButton
